ea5a7c036d4b441ba93210478ecb87938e2e0e3d
Jesse Glick <jglick@cloudbees.com>
2026-05-22 18:04:03 -0400

    Quieter bootstrap script

--- src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/scripts/container-bootstrap.sh
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/scripts/container-bootstrap.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Run by sh in the container to look for commands to run, which will be written the agent container to the workspace volume.
 # Must use only portable POSIX sh features, as the container may be running any distribution, even BusyBox.
-set -x # TODO while developing
 dir="$JENKINS_CONTAINER_WORK/$JENKINS_CONTAINER_NAME"
 # TODO parallelize
 while true
@@ -10,9 +9,12 @@ do
   do
     if test -f "$procdir/script.sh" -a \! -f "$procdir/seen"
     then
+      echo "Handling $procdir"
       touch "$procdir/seen"
       sh "$procdir/script.sh" <&- >"$procdir/out.txt" 2>"$procdir/err.txt"
-      echo $? >"$procdir/status.txt"
+      r=$?
+      echo $r >"$procdir/status.txt"
+      echo "Completed $procdir with status $r"
     fi
   done
   sleep 1
