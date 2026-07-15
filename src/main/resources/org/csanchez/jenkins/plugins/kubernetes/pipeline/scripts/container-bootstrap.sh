485e71fa7499b867f2a7147f5424435c67e7c7ca
Jesse Glick <jglick@cloudbees.com>
2026-07-15 15:43:01 -0400

    Write `status.txt` atomically https://github.com/jenkinsci/kubernetes-plugin/pull/2837#discussion_r3587235733

--- src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/scripts/container-bootstrap.sh
+++ src/main/resources/org/csanchez/jenkins/plugins/kubernetes/pipeline/scripts/container-bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Run by sh in the container to look for commands to run, which will be written the agent container to the workspace volume.
 # Must use only portable POSIX sh features, as the container may be running any distribution, even BusyBox.
+set -eu
 dir="$JENKINS_CONTAINER_WORK/$JENKINS_CONTAINER_NAME"
 # TODO parallelize
 while true
@@ -13,7 +14,8 @@ do
       touch "$procdir/seen"
       `cat "$procdir/shell.txt"` "$procdir/script.sh" <&- >"$procdir/out.txt" 2>"$procdir/err.txt"
       r=$?
-      echo $r >"$procdir/status.txt"
+      echo $r >"$procdir/status.txt.tmp"
+      mv "$procdir/status.txt.tmp" "$procdir/status.txt"
       echo "Completed $procdir with status $r"
     fi
   done
